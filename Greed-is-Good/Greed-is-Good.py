c7a8c1933101daa06bab7c1dcee4a11a39d6e7a0
EdenSolll <erg9359g.rit.edu>
2023-09-24 01:38:58 -0400

    codewars practice

--- Greed-is-Good/Greed-is-Good.py
+++ Greed-is-Good/Greed-is-Good.py
@@ -1,2 +1,21 @@
-def score(dice)
-  return
+def score(dice):
+    rolls = {}
+    total = 0
+    for val in dice:
+        if val in rolls:
+            rolls[val] += 1
+        else:
+            rolls[val] = 1
+    for val in rolls:
+        if rolls[val] >= 3:
+            if val == 1:
+                total += 10 * 100 
+            else:
+                total += val * 100
+            rolls[val] -= 3
+        if val == 1:
+            total += 100 * rolls[val]
+        if val == 5:
+            total += 50 * rolls[val]
+    return total
+
